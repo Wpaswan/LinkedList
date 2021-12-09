17e0c5cd79eea6485a97d2180fc6c933a4715b43
Wpaswan <dablumcapaswan@gmail.com>
2021-12-09 08:24:26 -0800

    [Dablu,modified the code to insert node between two]

--- LinkedListApplicaton/Program.cs
+++ LinkedListApplicaton/Program.cs
@@ -1,9 +1,9 @@
 ﻿using LinkedListApplicaton;
 LinkedList list = new LinkedList();
 list.Add(56);
-list.Add(30);
+//list.Add(30);
 list.Add(70);
-//list.InsertAtParticularPosition(2,30);
+list.InsertAtParticularPosition(2,30);
 LinkedList list2 = new LinkedList();
 //Appends the Nodes
 list2.AddFirst(56);
