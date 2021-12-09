9ee6640f7079bf93b7e40c26e37827825d77581d
Wpaswan <dablumcapaswan@gmail.com>
2021-12-09 08:45:22 -0800

    [Dablu,modified the code to remove last node from linked list]

--- LinkedListApplicaton/LinkedList.cs
+++ LinkedListApplicaton/LinkedList.cs
@@ -94,5 +94,19 @@ namespace LinkedListApplicaton
             this.head = this.head.next;
             return this.head;
         }
+        internal Node RemovaLastNode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
     }
 }
--- LinkedListApplicaton/Program.cs
+++ LinkedListApplicaton/Program.cs
@@ -13,5 +13,14 @@ list2.AddFirst(70);
 //Remove first node 50 from the linked list
 list2.RemoveFirstNode();
 list2.Display();
+//Removed Last Node 70 from the linked list
+Console.WriteLine("Removed the last node from the linked list");
+LinkedList list5 = new LinkedList();
+list5.Add(56);
+list5.Add(30);
+list5.Add(70);
+list5.RemovaLastNode();
+list5.Display();
 Console.ReadKey();
+
 
