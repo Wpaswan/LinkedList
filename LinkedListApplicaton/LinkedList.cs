b9a65fbbeb1778daa37f3feaf2e10b8a182a7e54
Wpaswan <dablumcapaswan@gmail.com>
2021-12-09 08:09:39 -0800

    [Dablu,modified the code  appended 30 and 70 to 56 in linked list]

--- LinkedListApplicaton/LinkedList.cs
+++ LinkedListApplicaton/LinkedList.cs
@@ -8,7 +8,7 @@ namespace LinkedListApplicaton
 {
     internal class LinkedList
     {
-        internal Node head; //new 
+        internal Node head; //new
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -17,27 +17,45 @@ namespace LinkedListApplicaton
             else
             {
                 Node temp = head;
-                while (temp.next!=null)
+                while (temp.next != null)
                 {
                     temp = temp.next;
                 }
                 temp.next = node;
             }
             Console.WriteLine("{0} inserted into the linked list", node.data);
+
+        }
+        internal void AddFirst(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the Append linked list", node.data);
         }
         internal void Display()
         {
             Node temp = this.head;
             if (temp == null)
             {
-                Console.WriteLine("Linked list is empty");
+                Console.WriteLine("\nLinked list is empty");
                 return;
             }
-            while (temp!=null)
+            while (temp != null)
             {
-                Console.Write(temp.data+" ");
+                Console.Write(temp.data + " ");
                 temp = temp.next; //temp=null
             }
+            Console.WriteLine("\n");
         }
     }
 }
--- LinkedListApplicaton/Program.cs
+++ LinkedListApplicaton/Program.cs
@@ -4,6 +4,13 @@ list.Add(56);
 list.Add(30);
 list.Add(70);
 //list.InsertAtParticularPosition(2,30);
+LinkedList list2 = new LinkedList();
+//Appends the Nodes
+list2.AddFirst(56);
+list2.AddFirst(30);
+list2.AddFirst(70);
+list2.Display();
+Console.ReadKey();
 list.Display();
 Console.ReadKey();
 
